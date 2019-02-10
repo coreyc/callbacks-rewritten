9469a1cc1de484b30b4fa64500409fe6dfe383e3
coreyc <ctc272@nyu.edu>
2019-02-10 14:28:24 -0600

    cb almost working

---
+++ callback.js
@@ -0,0 +1,72 @@
+const fs = require('fs')
+
+const inputdir = './files/'
+const completeddir = './completed/'
+
+// read directory
+// read each file
+// add property: "processed", set as true
+// save file to "completed" dir
+
+// read directory
+fs.readdir(inputdir, (err, files) => {
+  if (err) console.log(`Error reading directory ${inputdir}: ${err}`)
+  else {
+    for (const filename of files) {
+      // read each file
+      readJSONFile(inputdir + filename, (err, content) => {
+        if (err) {
+          console.log(`Error reading file ${filename}: ${err}`)
+        } else {
+          // add property: "processed", set as true
+          // save file to "completed" dir
+          const modifiedContent = Object.assign({processed: true}, content)
+          
+          writeJSONFile(completeddir + filename, modifiedContent, (err, data) => {
+            if (err) console.log(`Error writing file ${filename}: ${err}`)
+            else {
+              console.log(`${filename} processed and moved successfully`)
+            }
+          })
+        }
+      })
+    }
+  }
+})
+
+const readJSONFile = (filename, callback) => {
+  fs.readFile(filename, 'utf8', (err, content) => {
+    if (err) return callback(err)
+    else {
+      let parsedJson
+      let parseError
+      
+      try {
+        parsedJson = JSON.parse(content)
+      } catch(e) {
+        parseError = e
+      }
+
+      return callback(parseError, parsedJson)      
+    }
+  })
+}
+
+const writeJSONFile = (filename, content, callback) => {
+  let json
+  let parseError
+  
+  try {
+    json = JSON.stringify(content)
+  } catch(e) {
+    parseError = e
+  }
+
+  if (parseError) return callback(parseError)
+  else {
+    fs.writeFile(filename, json, (err) => {
+      if (err) return callback(err)
+      else return callback(filename)
+    })
+  }
+}
